b6fc19d78e79668ef33dafef2e24d4328990930a
bielwolf <gabryellobo@gmail.com>
2025-07-02 13:11:32 -0300

    controle de acesso

--- servidor/socket-back.js
+++ servidor/socket-back.js
@@ -5,10 +5,18 @@ import registrarEventosCadastro from "./registrarEventos/cadastro.js";
 import registrarEventosLogin from "./registrarEventos/login.js";
 
 import io from "./servidor.js";
+import autorizarUsuario from "./middlewares/autorizarUsuario.js";
 
-io.on("connection", (socket) => {
-  registrarEventosDocumento(socket,io);
-  registrarEventosInicio(socket,io);
+const nspUsuarios = io.of("/usuarios");
+
+nspUsuarios.use(autorizarUsuario);
+
+nspUsuarios.on("connection", (socket) => {
+  registrarEventosInicio(socket,nspUsuarios);
+  registrarEventosDocumento(socket,nspUsuarios);
+});
+
+io.of("/").on("connection", (socket) => {
   registrarEventosCadastro(socket,io);
   registrarEventosLogin(socket, io);
 });
